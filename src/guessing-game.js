e792412416b12e01d6d638ce5fc854c8873e0c37
ksenia-mahilnaya <ksenia.vlady@gmail.com>
2017-01-28 14:39:32 +0200

    89/11 passed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
+        
+        
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
-
+    
+    return Math.floor((this.min + this.max)/2)+1;
     }
 
     lower() {
-
+        this.setRange(this.min, Math.floor((this.min + this.max)/2));
+        this.guess();
+       
+        
     }
 
     greater() {
-
+        
+        this.setRange(Math.floor((this.min + this.max)/2)+1, this.max);
+        this.guess();
     }
 }
 
